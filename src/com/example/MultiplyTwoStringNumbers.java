1e6dc03dc9eccb6caeae373f5797aa3830bfdf45
Shreya <shreya.p@peerbits.com>
2022-11-21 17:07:25 +0530

    Apply New Logic to multiplyTwoStringNumbers

--- src/com/example/MultiplyTwoStringNumbers.java
+++ src/com/example/MultiplyTwoStringNumbers.java
@@ -6,28 +6,57 @@ import java.util.Scanner;
 E.g. String A = 25 and string B = 125 , result = 3125. Don’t use string.parseInt or Integer.valueof functions.*/
 
 public class MultiplyTwoStringNumbers {
-	
-	public static String getMultiplicationOfTwoStringNumber(String stringNumberOne,String stringNumberTwo) {
-		
-		return String.valueOf((Integer.decode(stringNumberOne)*Integer.decode(stringNumberTwo)));
+
+	private static String convertIntegerToString(int result) {
+
+		String res = "";
+
+			while (result>0){
+			int digitValue = result % 10;
+			char digit = (char) ('0' + digitValue);
+			res = digit + res;
+			result = result / 10;
+		}
+
+		return res;
 	}
-	
-	
+
+
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Please Enter the String Number : ");
+		System.out.print("Please Enter the First Number : ");
 		
-		String stringNumberOne = sc.next();
+		String number1 = sc.next();
+
+		System.out.print(" \n Please Enter the Second Number : ");
 		
-		System.out.print(" \n Please Enter the String Number : ");
-		
-		String stringNumberTwo = sc.next();
-		
-		System.out.println("Two String Number Multiplication is : "+stringNumberOne+" * "+stringNumberTwo+" = "
-		+MultiplyTwoStringNumbers.getMultiplicationOfTwoStringNumber(stringNumberOne, stringNumberTwo));
+		String number2 = sc.next();
+
+		String output = "0";
+		if(number1.equalsIgnoreCase("0") || number1.equalsIgnoreCase("0")){
+			System.out.println("Two String Number Multiplication is : " + number1 + " * " + number2 + " = "
+					+ output);
+			return;
+		}
+
+		 output = convertIntegerToString(convertStringToInteger(number1) * convertStringToInteger(number2));
+
+			System.out.println("Two String Number Multiplication is : " + number1 + " * " + number2 + " = "
+					+ output);
 				
+	}
+
+	private static int convertStringToInteger(String number) {
+
+		int sum = 0;
+
+			for (int i = 0; i < number.length(); i++) {
+
+				sum = (sum * 10) + (number.charAt(i) - '0');
+			}
+		return sum;
 	}
 
 }
